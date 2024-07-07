8049ecb86dd85da1c58901bb6828fb2055b8a6c3
unknown <bayram43kocaman@gmail.com>
2024-07-07 18:24:16 +0400

    js-lesson-task-update

---
+++ js_LessionsTask1/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+// let roadLong = prompt(" bakiden sabirabda nece km yoldur?")
+
+// let convert= user*1000 
+
+
+
+// console.log(convert+"km")
+
+
+
+// let salary = prompt(" dolari nece manata ceviremek isteyirsen")
+
+// let convert= user * 1.7
+
+// console.log(convert + "azn")
+
+
+
+
+// let azn = prompt("manati daxil edin");
+// let rubl = prompt("rublu daxil edin");
+
+// let rubConvertMoney = rubl * 0.011
+
+// let aznConvertMoney= azn  * 0.59
+
+// console.log("rubl to dollar: ",aznConvertMoney);
+// console.log("manat to dollar: ",rubConvertMoney);
+
+
+
---
+++ js_lessonTask2/app.js
@@ -0,0 +1,333 @@
+// let a=1
+
+
+
+// let b=3
+
+
+// if(a%2==0){
+//     console.log(`${a} even num`)
+
+// }else {
+//     console.log(`${a} odd num`)
+// }
+
+
+
+
+// let call_fee= +prompt("Zeng Haqqinizi daxil edin: ")
+// let operator = prompt("zehmet olmasa operatoru seciniz (Bakcell ,Azarcell ,Nar): ");
+// let call_fee_calculation
+
+// if(operator =="Bakcell"){
+//     call_fee_calculation=call_fee*0.5
+//     console.log("Bakcell",call_fee_calculation)
+// }else if(operator == "Azarcell"){
+//     call_fee_calculation=call_fee*0.8
+//     console.log("Bakcell",call_fee_calculation)
+// }else if(operator =="Nar" ){
+//     call_fee_calculation=call_fee*0.8
+//     console.log("Nar",call_fee_calculation)
+// }
+
+
+
+// let numberavreage= +prompt("your just 1 to 100 avreage number write!!: ")
+
+// if(numberavreage>=1 && numberavreage <= 100){
+
+//     if(numberavreage % 2==0 && numberavreage %6==0){
+//         console.log("fiz buzz : ",numberavreage)
+    
+//     }else if(numberavreage % 4==0){
+//         console.log("fizz: ",numberavreage)
+        
+    
+//     }else if(numberavreage % 5==0){
+//         console.log('buzz: ',numberavreage)
+//     }
+//     else{
+//         console.log("please  4 to 5 divided number : ",numberavreage)
+     
+//     }
+
+// }else{
+//     console.log(`wrong choose !!!`)
+// }
+
+
+// let Sixhali = +prompt("Sixhali age enter")
+// let Feqan = +prompt(" Feqan age enter")
+
+
+
+
+// if(Sixhali >=18){
+//     console.log(" Şixəli bey sizin yasniz prava almaga catir: ",Sixhali)
+// }else{
+//     console.log("sizin yasniz catmir hele usaqsiniz")
+// }
+
+// if(Feqan >=18){
+//     console.log("Feqan bey sizin yasniz prava almaga catir: ",Feqan)
+// }else{
+//     console.log("Feqan sizin yasniz catmir hele usaqsiniz")
+// }
+
+
+
+
+// Task 1 ---
+
+
+
+
+// let weekdays= prompt(" week days write just 1 to 7 number : ")
+
+
+
+// if(weekdays==1){
+//     console.log("Monday")
+// }else if (weekdays==2){
+//     console.log("Tuesday")
+
+// }else if(weekdays==3){
+//     console.log("Wednesday")
+// }else if (weekdays==4){
+//     console.log("Thursday")
+// }else if(weekdays==5){
+//     console.log("Friday")
+// }else if(weekdays==6){
+//     console.log("Saturday")
+// }else if(weekdays==7){
+//     console.log("Sunday")
+// }else{
+//     console.error("wrong choose please coorect choose make!! (1 to 7)")
+// }
+
+
+
+
+//task 2
+
+
+
+
+// let weekdays= prompt(" moon days write just 1 to 12 number : ")
+
+
+
+// if(weekdays==1){
+//     console.log("January")
+// }else if (weekdays==2){
+//     console.log("February")
+// }else if(weekdays==3){
+//     console.log("March")
+// }else if (weekdays==4){
+//     console.log("April")
+// }else if(weekdays==5){
+//     console.log("May")
+// }else if(weekdays==6){
+//     console.log("June")
+// }else if(weekdays==7){
+//     console.log("July")
+// }else if(weekdays==8){
+//     console.log("August")
+// }else if(weekdays==9){
+//     console.log("September")
+// }else if(weekdays==10){
+//     console.log("October")
+// }else if(weekdays==11){
+//     console.log("November")
+// }else if(weekdays==12){
+//     console.log("December")
+// }else{
+//     console.error("wrong choose please coorect choose make!! (1 to 12)")
+// }
+
+
+
+//task 3
+
+
+// let gender = prompt("gender enter")
+
+
+
+// if(gender == "male"){
+//     console.log("welcome male")
+// }else if(gender == "man"){
+//     console.log("welcome By man")
+// }else{
+//     console.log("Just male/man gender write")
+// }
+
+
+
+
+
+// task 4
+
+// --------------------------------------------------------
+
+
+
+// let name = prompt("Name enter")
+// let gender = prompt("Gender enter")
+// let age = +prompt("Age enter")
+
+
+
+//girl
+
+
+// 1-nci woy
+
+// if(gender == "female" && age >=18){
+//     console.log(`welcome Monday Party ${name}-lady`)
+// }else {
+//     console.log(`${name} your age not enough this party :( ${age}`)
+// }
+
+
+// man
+
+// if(gender == "male" && age >=18){
+//     console.log(`welcome Monday Party ${name}-sir`)
+// }else {
+//     console.log(`${name} your age not enough this party :( ${age}`)
+// }
+
+
+// 2-nci woy
+
+
+// if(gender == "female" && age >=18){
+//     console.log(`welcome Monday Party ${name}-lady`)
+// }else if(gender == "male" && age >=18){
+//     console.log(`welcome Monday Party ${name}-sir`)
+
+// }else{
+//     console.log(` ${name} your age not enough this party :( ${age} age`)
+// }
+ 
+// 3-nci woy
+
+
+// if(age>=18){
+//     if(gender == "female"){
+//         console.log(`welcome Monday Party ${name}-lady`)
+//     }else if (gender =="male"){
+//         console.log(`welcome Monday Party ${name}-Sir`)
+//     }else{
+//         console.log(`not correct gender : ${gender}`)
+//     }
+// }else{
+//     console.log(` ${name} your age not enough this party :( ${age} age`)
+// }
+
+
+
+
+
+
+// task 5
+
+// --------------------------------------------------------
+
+
+
+// let home_Square = +prompt("Home Square Enter: ");
+
+// let home_Price =  +prompt("Home Price Enter: ");
+
+// let currency = prompt("Currency Enter: ");
+
+
+// if(home_Square > 2000 && home_Price === 99999 && currency === "Azn"){
+//     console.log(`I buy the house `)
+// }else{
+//     console.log(" I don't buy house broker don't worried")
+// }
+
+
+
+// task 6
+// --------------------------------------------------------
+
+
+// let car_Number= prompt("Car Number plate Enter:   ");
+
+
+
+// if(car_Number === "AZ"){
+//     console.log(`${car_Number}: this Azerbaijan Car number `)
+// }else if(car_Number === "TR"){
+//     console.log(`${car_Number}: this Turkish Car number `)
+
+// }else if(car_Number ==="DE"){
+//     console.log(`${car_Number}: this Germany Car number `)
+
+// }else if(car_Number ==="RU"){
+//     console.log(`${car_Number}: this Russia Car number `)
+
+// }else if(car_Number === "UA"){
+//     console.log(`${car_Number}: this Ukrainan Car number `)
+
+// }else if(car_Number === "ZH"){
+//     console.log(`${car_Number}: this Switzerland Car number `)
+// }else if(car_Number === "USA"){
+//     console.log(`${car_Number}: this United states Car number `)
+
+// }else{
+//     console.log(`unknown country car plate number:${car_Number} `)
+// }
+
+
+// task 7
+// --------------------------------------------------------
+
+let Cam_infermation =alert("kamera isitfade qaydaları sadecu bu nömerler ile baxa bilersiz evin kamerasin\n"+
+                           "1-darvaza \n"+
+                           "2-heyit\n"+
+                           "3-qarac\n"+
+                           "4-baxça\n"+
+                           "5-zall\n"+
+                           "6-otaq 1\n"+
+                           "7-otaq 2\n"+
+                           "8-otaq 3\n"+
+                           "9-otaq 4\n"+
+                           "10-otaq 5\n") 
+let home_cam=+prompt("Welcome cam app Just 1 to 10 number choose and");
+
+if(home_cam==1){
+    console.log("darvaza ")
+}else if(home_cam==2){
+    console.log("heyit")
+}else if(home_cam==3){
+    console.log("qarac")
+}else if(home_cam==4){
+    console.log("baxça")
+}else if(home_cam==5){
+    console.log("zall")
+}else if(home_cam ==6){
+    console.log("otaq 1")
+}else if(home_cam==7){
+    console.log("otaq 2")
+}else if(home_cam==8){
+    console.log("otaq 3")
+}else if(home_cam==9){
+    console.log("otaq 4")
+}else if(home_cam==10){
+    console.log("otaq 5")
+}else{
+    console.log('zehmet olmasa size uygun edilmis kamera reqemleri ile  seçeniz (1-10)')
+}
+
+
+
+
+
+
+
+
