510e6cf24ea0355343e03bef44f37430c93880fa
unknown <bayram43kocaman@gmail.com>
2024-07-08 00:24:51 +0400

    update atm js task

--- js_lessonTask2/app.js
+++ js_lessonTask2/app.js
@@ -287,46 +287,91 @@
 // task 7
 // --------------------------------------------------------
 
-let Cam_infermation =alert("kamera isitfade qaydaları sadecu bu nömerler ile baxa bilersiz evin kamerasin\n"+
-                           "1-darvaza \n"+
-                           "2-heyit\n"+
-                           "3-qarac\n"+
-                           "4-baxça\n"+
-                           "5-zall\n"+
-                           "6-otaq 1\n"+
-                           "7-otaq 2\n"+
-                           "8-otaq 3\n"+
-                           "9-otaq 4\n"+
-                           "10-otaq 5\n") 
-let home_cam=+prompt("Welcome cam app Just 1 to 10 number choose and");
+// let Cam_infermation =alert("kamera isitfade qaydaları sadecu bu nömerler ile baxa bilersiz evin kamerasin\n"+
+//                            "1-darvaza \n"+
+//                            "2-heyit\n"+
+//                            "3-qarac\n"+
+//                            "4-baxça\n"+
+//                            "5-zall\n"+
+//                            "6-otaq 1\n"+
+//                            "7-otaq 2\n"+
+//                            "8-otaq 3\n"+
+//                            "9-otaq 4\n"+
+//                            "10-otaq 5\n") 
+// let home_cam=+prompt("Welcome cam app Just 1 to 10 number choose and");
 
-if(home_cam==1){
-    console.log("darvaza ")
-}else if(home_cam==2){
-    console.log("heyit")
-}else if(home_cam==3){
-    console.log("qarac")
-}else if(home_cam==4){
-    console.log("baxça")
-}else if(home_cam==5){
-    console.log("zall")
-}else if(home_cam ==6){
-    console.log("otaq 1")
-}else if(home_cam==7){
-    console.log("otaq 2")
-}else if(home_cam==8){
-    console.log("otaq 3")
-}else if(home_cam==9){
-    console.log("otaq 4")
-}else if(home_cam==10){
-    console.log("otaq 5")
+// if(home_cam==1){
+//     console.log("darvaza ")
+// }else if(home_cam==2){
+//     console.log("heyit")
+// }else if(home_cam==3){
+//     console.log("qarac")
+// }else if(home_cam==4){
+//     console.log("baxça")
+// }else if(home_cam==5){
+//     console.log("zall")
+// }else if(home_cam ==6){
+//     console.log("otaq 1")
+// }else if(home_cam==7){
+//     console.log("otaq 2")
+// }else if(home_cam==8){
+//     console.log("otaq 3")
+// }else if(home_cam==9){
+//     console.log("otaq 4")
+// }else if(home_cam==10){
+//     console.log("otaq 5")
+// }else{
+//     console.log('zehmet olmasa size uygun edilmis kamera reqemleri ile  seçeniz (1-10)')
+// }
+
+
+
+// task 7
+// --------------------------------------------------------
+
+// 1-bütcəni gösdər
+// 2- pulu çıxartmaq
+// 3- pulu yatırmaq
+// 4- sistemden çıxmaq
+
+
+
+
+let UserAmount=2000
+
+
+let atm_Infermation=alert("1-bütcəni gösdər\n"+
+                           "2- pulu çıxartmaq\n"+
+                           "3- pulu yatırmaq\n"+
+                           "4- sistemden çıxmaq")
+
+
+let userInport=+prompt("zehmet olmasa seçim ediniz (1-4)-aralığında rəqəm seçiniz: ")
+
+
+if(userInport==1){
+    console.log(`Sizin Məbləğiniz: ${UserAmount} `)
+}else if(userInport==2){
+    let withdraw_money_User_input=+prompt("zehmet olmasa çıxarmaq isdediyiniz mevacibinizi yazin: ");
+    if(withdraw_money_User_input<=UserAmount){
+        UserAmount-=withdraw_money_User_input
+        console.log("pulu çıxartmaq uğrlu keçdi: ",UserAmount)
+
+    }else{
+        console.log(`Hesabnızda bu qədər ${withdraw_money_User_input} pul yoxdur`)
+    }
+    
+}else if(userInport==3){
+    let money_deposite= +prompt(` zehmet olmasa hesabinza daxil edlicey meblegi yaziniz: `)
+    UserAmount +=money_deposite
+    console.log("Hesabinizda guncel mebleginiz : ",UserAmount)
+    
+
+}else if(userInport==4){
+    console.log("Hesabininzdan çıxış olunur ...")
 }else{
-    console.log('zehmet olmasa size uygun edilmis kamera reqemleri ile  seçeniz (1-10)')
+    console.log("düzgun seçim edniniz: ",userInport)
 }
-
-
-
-
 
 
 
